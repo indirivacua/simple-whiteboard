0ea2821c71712c8dc267a5712b2e91962b300f84
Oscar Stanchi <indirivacua@hotmail.com>
2023-07-19 11:53:44 -0300

    load example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { getStroke } from "perfect-freehand";
+import { getSvgPathFromStroke } from "./utils";
+import "./styles.css";
 
-function App() {
+const options = {
+  size: 32,
+  thinning: 0.5,
+  smoothing: 0.5,
+  streamline: 0.5,
+  easing: (t) => t,
+  start: {
+    taper: 0,
+    easing: (t) => t,
+    cap: true
+  },
+  end: {
+    taper: 100,
+    easing: (t) => t,
+    cap: true
+  }
+};
+
+export default function Example() {
+  const [points, setPoints] = React.useState([]);
+
+  function handlePointerDown(e) {
+    e.target.setPointerCapture(e.pointerId);
+    setPoints([[e.pageX, e.pageY, e.pressure]]);
+  }
+
+  function handlePointerMove(e) {
+    if (e.buttons !== 1) return;
+    setPoints([...points, [e.pageX, e.pageY, e.pressure]]);
+  }
+
+  const stroke = getStroke(points, options);
+  const pathData = getSvgPathFromStroke(stroke);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      style={{ touchAction: "none" }}
+    >
+      {points && <path d={pathData} />}
+    </svg>
   );
 }
-
-export default App;
